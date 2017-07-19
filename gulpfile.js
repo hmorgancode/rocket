d6163ed95b33e6ced2ea13f73524dd3c231cebcf
Henry Morgan <hmorgan471@gmail.com>
2017-07-19 15:55:05 -0400

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const babel = require('gulp-babel');
 gulp.task('default', () => {
   return gulp.src('src/**/*.js')
     .pipe(babel({
-      presets: ['transform-es2015-modules-commonjs']
+      plugins: ['transform-es2015-modules-commonjs']
     }))
     .pipe(gulp.dest('dist'));
 });
