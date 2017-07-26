32aa56e9abfb91b1f1c23d8d3b33e6dbed3c379e
Henry Morgan <hmorgan471@gmail.com>
2017-07-26 17:17:09 -0400

    add initial frontend build and deploy through static file system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const promisify = require('util').promisify;
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const watch = require('gulp-watch');
+const git = require('gulp-git');
+// const yarn = require('gulp-yarn');
+const run = require('gulp-run');
 // you should be able to remove this after gulp 4.0
 const plumber = require('gulp-plumber');
 
@@ -14,9 +18,39 @@ gulp.task('babel', async () => {
     .pipe(gulp.dest('dist'));
 });
 
+// try switching over to just gulp.watch later?
 gulp.task('watch_babel', async () => {
   return watch('src/**/*.js')
     .pipe(plumber())
     .pipe(babel())
     .pipe(gulp.dest('dist'));
+});
+
+// gulp.task('install_packages', async () => {
+//   return gulp.src(['./package.json', './yarn.lock'])
+//     .pipe(yarn());
+// });
+
+// bash commands are better suited to a makefile and/or Jenkins or some other CI,
+// but for now just do this stuff in gulp
+gulp.task('build_groot', async () => {
+  await promisify(git.updateSubmodule)({ args: '--init' });
+  // return run('cd groot && yarn install && yarn build').exec();
+  // run(...).exec() is supposed to return a stream, but isn't?
+  // and yarn is adding node_modules/gulp-run as a symlink, but it can't be followed?
+  // uh, learn what's going on later, or just use CI for this
+  const build = new run.Command('cd groot && yarn install && yarn build');
+  build.exec = promisify(build.exec);
+  console.log('Building groot for production...');
+  return build.exec();
+});
+
+gulp.task('deploy', ['babel', 'build_groot'], async () => {
+  // hacky bash because this is temporary. (create something in dist/public to avoid error when no files are present)
+  const prep = new run.Command('touch dist/public/foo && rm -r dist/public/* && cp -r groot/build/* dist/public/');
+  prep.exec = promisify(prep.exec);
+  console.log('Preparing static files...');
+  return prep.exec();
+  // move build to root?
+  // forever? pm2?
 });
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import koa from 'koa';
 import koaHelmet from 'koa-helmet';
 import koaRouter from 'koa-router';
 import koaBodyParser from 'koa-bodyparser';
+import koaStatic from 'koa-static';
 import { graphqlKoa, graphiqlKoa  } from 'apollo-server-koa';
 import schema from './data/graphql/schema';
 import { connectToDatabase } from './modules/databaseConnection';
@@ -13,22 +14,24 @@ connectToDatabase(process.env.TEST_DB_URI);
 
 const app = new koa();
 const router = new koaRouter();
-const PORT = 3000;
 
+// set various more secure http headers
 app.use(koaHelmet());
+// serve public directory
+app.use(koaStatic(__dirname + process.env.PUBLIC_DIR));
 
 // apollo client only uses post. See apollo-client issue #813 for context.
 router.post('/graphql', koaBodyParser(), graphqlKoa({ schema }));
 
 router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
 
-router.use('/', async (ctx, next) => {
-  ctx.body = 'You are messing around with koa stuff and should remove this. (404 is koa\'s default response status)';
-  await next(); // not needed anymore? mess around more w/ koa routes
-});
+// router.use('/', async (ctx, next) => {
+//   ctx.body = 'You are messing around with koa stuff and should remove this. (404 is koa\'s default response status)';
+//   await next(); // not needed anymore? mess around more w/ koa routes
+// });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.listen(PORT);
+app.listen(process.env.PORT);
 
-export default app; // for tests?
+// export default app; // for tests?
