c2455a82a6f008142f4d3d94c905555c2bf271a0
Henry Morgan <hmorgan471@gmail.com>
2017-10-05 15:48:01 -0400

    remove unused test files

--- src/modules/imageDelete.test.js
+++
@@ -1,4 +0,0 @@
-// quick test failure as a reminder
-test('tests_exist', () => {
-  expect('image delete endpoint tests').toBe(true);
-});
--- src/modules/imageUpload.test.js
+++
@@ -1,4 +0,0 @@
-// quick test failure as a reminder
-test('tests_exist', () => {
-  expect('image upload endpoint tests').toBe(true);
-});
